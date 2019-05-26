c360fdd5a852391e99e7742a63e9662e56bdf386
Ganesh Pol <polganesh@gmail.com>
2019-05-26 07:05:23 +0200

    remove not required variables

--- cloud/aws/computing/orchestration/eks/3-sg_eks_node.tf
+++ cloud/aws/computing/orchestration/eks/3-sg_eks_node.tf
@@ -1,7 +1,7 @@
     resource "aws_security_group" "tf-eks-node" {
         name        = "terraform-eks-node"
         description = "Security group for all nodes in the cluster"
-        vpc_id      = "${var.vpc_id}"
+        vpc_id      = "${data.aws_vpc.vpc.id}"
 
         egress {
             from_port   = 0
