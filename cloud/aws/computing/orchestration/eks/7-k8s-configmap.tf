4d5c2fc9a66d5038b71f582e5cbc0d3aec54e723
Ganesh Pol <polganesh@gmail.com>
2019-05-26 06:46:13 +0200

    changes for config map

---
+++ cloud/aws/computing/orchestration/eks/7-k8s-configmap.tf
@@ -0,0 +1,18 @@
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.wnode.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+}
