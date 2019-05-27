44136fd4abf31ac89a929b38117ca19ec51767d9
Ganesh Pol <polganesh@gmail.com>
2019-05-27 12:25:39 +0200

    changes in command for applying tag

--- cloud/aws/computing/orchestration/eks/8-tag-subnets.tf
+++ cloud/aws/computing/orchestration/eks/8-tag-subnets.tf
@@ -3,11 +3,11 @@
 # kubernetes.io/role/elb 1 for external load balancers
 resource "null_resource" "tag-public-subnet" {
 	provisioner "local-exec" {
-		command = "aws create-tags --resources ${element(data.aws_subnet_ids.public_subnets.ids, count.index)} --tags Key=kubernetes.io/role/elb,Value=5"
+		command = "aws ec2 create-tags --resources ${element(data.aws_subnet_ids.public_subnets.ids, count.index)} --tags Key=kubernetes.io/role/elb,Value=5"
 	}
 	
 	provisioner "local-exec" {
-		command = "aws create-tags --resources ${element(data.aws_subnet_ids.public_subnets.ids, count.index)} --tags Key=kubernetes.io/cluster/eks-${var.region_id}-${var.environment}-${var.cost_centre}-vpc${var.vpc_seq_id}-${var.app_service}-${var.seq_id},Value=shared-test"
+		command = "aws ec2 create-tags --resources ${element(data.aws_subnet_ids.public_subnets.ids, count.index)} --tags Key=kubernetes.io/cluster/eks-${var.region_id}-${var.environment}-${var.cost_centre}-vpc${var.vpc_seq_id}-${var.app_service}-${var.seq_id},Value=shared-test"
 	}
 	
 	depends_on = ["aws_eks_cluster.main"]
