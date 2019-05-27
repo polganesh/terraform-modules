bc296c747db2499ddf2ed886e1e1cb6da006e83e
Ganesh Pol <polganesh@gmail.com>
2019-05-27 12:57:31 +0200

    tag private subnet

--- cloud/aws/computing/orchestration/eks/8-tag-subnets.tf
+++ cloud/aws/computing/orchestration/eks/8-tag-subnets.tf
@@ -1,4 +1,4 @@
-# it is important that all public subnets must be tagged with keys 
+# it is important that all public subnets must be tagged with following keys. these keys help EKS to create  external load balancer in public subnet
 # kubernetes.io/cluster/<cluster-name> shared 
 # kubernetes.io/role/elb 1 for external load balancers
 resource "null_resource" "tag-public-subnet" {
@@ -8,6 +8,17 @@ resource "null_resource" "tag-public-subnet" {
 	}
 	provisioner "local-exec" {
 		command = "aws ec2 create-tags --resources ${element(data.aws_subnet_ids.public_subnets.ids, count.index)} --tags Key=kubernetes.io/cluster/eks-${var.region_id}-${var.environment}-${var.cost_centre}-vpc${var.vpc_seq_id}-${var.app_service}-${var.seq_id},Value=shared"
+	}
+	depends_on = ["aws_eks_cluster.main"]
+}
+
+
+# it is important that all private app subnets must be tagged with following key.it will help k8s to add EC2 instances in these subnets to add in cluster.
+resource "null_resource" "tag-privateApp-subnet" {
+    count = "${length(data.aws_subnet_ids.private_app_subnets.ids)}"
+
+	provisioner "local-exec" {
+		command = "aws ec2 create-tags --resources ${element(data.aws_subnet_ids.private_app_subnets.ids, count.index)} --tags Key=kubernetes.io/cluster/eks-${var.region_id}-${var.environment}-${var.cost_centre}-vpc${var.vpc_seq_id}-${var.app_service}-${var.seq_id},Value=shared"
 	}
 	depends_on = ["aws_eks_cluster.main"]
 }
